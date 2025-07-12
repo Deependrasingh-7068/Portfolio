39a7274d28eef4fc40f9f9465289f1f0b0991a6d
Deependra Singh <harshramsingh768@gmail.com>
2025-07-12 23:43:56 +0530

    feat: some changes made

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,12 @@ import Projects from "./pages/Project";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import clsx from "clsx";
-import "./App.css"; //
+
+import "./css/App.css"; //
 import Image from "./Image"; // Adjust the import path as necessary
 
 function App() {
-  <Image
-    src="/src/assets/adobe Express - file (1).png" // Adjust the path as necessary
-        alt="Background Image"
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100vw",
-          height: "100vh",
-          objectFit: "cover",
-          zIndex: -1,
-          backgroundSize: "cover",
-          backgroundPosition: "left",
-          backgroundBlendMode: "normal",
-          backgroundRepeat: "no-repeat",
-          backgroundAttachment: "fixed",
-        }}
-  />;
+
   return (
     <Router>
       
