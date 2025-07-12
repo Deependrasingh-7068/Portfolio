10a308ecfa15c41fad2fdae9f3638eb5743f8d5a
Deependra Singh <harshramsingh768@gmail.com>
2025-07-13 00:33:47 +0530

    home css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+// src/Home.jsx
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Button } from "@/component/ui/Button";
 import Image from "@/Image";
-import "../css/Home.css";
+import "../CSS/Home.css"; 
+
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -32,28 +34,25 @@ function Home() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-gray-900 text-white home-container">
+    <div className=" home-container">
       {/* Navbar */}
-
+     
+     
       {/* Hero Section */}
       <section
         ref={heroRef}
-        className="flex flex-col items-center justify-center h-screen text-center"
+        className=" flex flex-col items-center justify-center h-screen text-center"
       >
         <h1 className="text-5xl font-extrabold main-name">Hi, I'm Deependra</h1>
         <p className="mt-4 text-xl">Frontend Developer | MERN Stack </p>
         <div className="mt-6 space-x-4">
           <Button className="btn-primary">Download Resume</Button>
-          <Button
-            variant="outline"
-            className="border-teal-500 text-teal-400 hover:bg-teal-700"
-          >
-            Contact Me
-          </Button>
+          <Button variant="outline" className="border-teal-500 text-teal-400 hover:bg-teal-700">Contact Me</Button>
         </div>
       </section>
 
       {/* About Section */}
+      
     </div>
   );
 }
